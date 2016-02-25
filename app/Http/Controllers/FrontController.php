467dbb285d4aec426c9bd9be4da58d393cedcfe6
feltoxXx <luisf1939@gmail.com>
2016-02-25 12:47:34 -0430

    Clase Vistas
    Añadido vistas, imagenes, js, css y nuevas rutas (eliminadas las
    anteriores)

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,42 @@
+<?php namespace cinema\Http\Controllers;
+
+use cinema\Http\Requests;
+use cinema\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return view('index');
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function contacto()
+	{
+		return view('contacto');
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function reviews()
+	{
+		return view('reviews');
+	}
+
+	
+
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,35 +11,6 @@
 |
 */
 
-
-Route::get('controlador','PruebaController@index');
-Route::get('name/{nombre}','PruebaController@nombre');
-
-Route::resource('movie', 'MovieController');
-
-
-//Rutas
-
-Route::get('prueba', function() {
-    return "Hola desde Routes.php";
-});
-
-//Pasar valor por get
-Route::get('nombre/{nombre}', function($nombre) {
-    return "Mi nombre es: ".$nombre;
-});
-
-//Pasar valor por default si no se especifica alguno
-Route::get('nombre/{nombre?}', function($nombre = "Especifica un nombre") {
-    return "Mi nombre es: ".$nombre;
-});
-
-
-Route::get('/', 'WelcomeController@index');
-
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+Route::get('/', 'FrontController@index');
+Route::get('contacto', 'FrontController@contacto');
+Route::get('reviews', 'FrontController@reviews');
