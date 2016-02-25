571e611c861fbe8a55074375bc36e684f45ff600
feltoxXx <luisf1939@gmail.com>
2016-02-25 10:29:32 -0430

    Clase Rutas
    Clase Rutas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,24 @@
 |
 */
 
+
+//Rutas
+
+Route::get('prueba', function() {
+    return "Hola desde Routes.php";
+});
+
+//Pasar valor por get
+Route::get('nombre/{nombre}', function($nombre) {
+    return "Mi nombre es: ".$nombre;
+});
+
+//Pasar valor por default si no se especifica alguno
+Route::get('nombre/{nombre?}', function($nombre = "Especifica un nombre") {
+    return "Mi nombre es: ".$nombre;
+});
+
+
 Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
